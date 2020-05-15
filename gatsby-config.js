a3dd6a3dd8abfe5d6488f976988936070073aeb1
Somoy khan <k.somoykhan@gmail.com>
2020-05-16 03:02:33 +0600

    removed sitemap & robot.txt plugins from theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,57 +1,56 @@
-module.exports = (options) => ({
-    siteMetadata: {
-        title: `Somoy Portfolio`,
-        author: {
-          name: `somoy khan`,
-          summary: `who lives and works in planet Earth builiding useful Full Stack Applications using Top notch Javascript Technologies.`,
-        },
-        description: `A starter portfolio demonstrating what I can do.`,
-        siteUrl: `https://elegant-khorana-6044ac.netlify.app/`,
-        social: {
-          twitter: `mdtsk`,
-        },
-      },
-    plugins: [
-        `gatsby-plugin-sharp`,
-        `gatsby-remark-images`,
-        `gatsby-plugin-react-helmet`,
-        `gatsby-plugin-robots-txt`,
-        `gatsby-plugin-sitemap`,
-        {
-            resolve: 'gatsby-source-filesystem',
+module.exports = options => ({
+  siteMetadata: {
+    title: 'Somoy Portfolio',
+    author: {
+      name: 'somoy khan',
+      summary:
+        'who lives and works in planet Earth builiding useful Full Stack Applications using Top notch Javascript Technologies.'
+    },
+    description: 'A starter portfolio demonstrating what I can do.',
+    siteUrl: 'https://elegant-khorana-6044ac.netlify.app/',
+    social: {
+      twitter: 'mdtsk'
+    }
+  },
+  plugins: [
+    'gatsby-plugin-sharp',
+    'gatsby-remark-images',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: options.contentPath || 'work',
+        name: 'work'
+      }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: options.contentPath || 'blog',
+        name: 'blog'
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        gatsbyRemarkPlugins: [
+          {
+            resolve: 'gatsby-remark-images',
             options: {
-                path: options.contentPath || 'work',
-                name: 'work'
+              maxWidth: 500,
+              sizeByPixelDensity: true
             }
-        },
-        {
-            resolve: 'gatsby-source-filesystem',
-            options: {
-                path: options.contentPath || 'blog',
-                name: 'blog'
-            }
-        },
-        {
-            resolve: 'gatsby-plugin-mdx',
-            options: {
-                gatsbyRemarkPlugins: [
-                    {
-                      resolve: `gatsby-remark-images`,
-                      options: {
-                        maxWidth: 500,
-                        sizeByPixelDensity: true,
-                      },
-                    },
-                  ],
-            }
-        },
-        {
-            resolve: "gatsby-plugin-react-svg",
-            options: {
-              rule: {
-                include: /.svg$/
-              }
-            }
+          }
+        ]
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /.svg$/
         }
-    ]
+      }
+    }
+  ]
 })
